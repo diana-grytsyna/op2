c73b9130616c57b3ea255b33c8c078531d629837
diana-grytsyna <89968360+diana-grytsyna@users.noreply.github.com>
2022-04-05 12:01:38 +0300

    Create func.hpp

---
+++ func.hpp
@@ -0,0 +1,10 @@
+#ifndef func_hpp
+#define func_hpp
+
+#include <stdio.h>
+#include <fstream>
+using namespace std;
+void create_file(string filename);
+void output_file(string filename);
+void create_new_file(string file1, string file2);
+#endif /* func_hpp */
